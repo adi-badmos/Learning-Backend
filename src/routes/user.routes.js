e1d41d9b335ea52bac7cee3eff6eb316150e4103
adi-badmos <adityapratap141003@gmail.com>
2026-08-22 18:26:46 +0530

    Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js";
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWt } from "../middlewares/auth.middleware.js";
 
@@ -15,11 +27,17 @@ router.route('/register').post(
     }]),
     registerUser
 );
-
 router.route('/login').post(loginUser);
 
 // Secured routes
 router.route('/logout').post(verifyJWt, logoutUser);
-router.route('/refresh-token').post(refreshAccessToken)
+router.route('/refresh-token').post(refreshAccessToken);
+router.route('/change-password').post(verifyJWt, changeCurrentPassword);
+router.route('/current-user').post(verifyJWt, getCurrentUser);
+router.route('/update-account').patch(verifyJWt, updateAccountDetails);
+router.route('/avatar').patch(verifyJWt, upload.single("avatar"), updateUserAvatar);
+router.route('cover-image').patch(verifyJWt, upload.single("coverImage"), updateUserCoverImage);
+router.route('/c/:username').get(verifyJWt, getUserChannelProfile);
+router.route('/history').get(verifyJWt, getWatchHistory);
 
 export default router;
